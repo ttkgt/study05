b8c22aa78862c073f7eb169af3e055005385a022
ttkgt <ttkgt86@gmail.com>
2018-08-19 20:26:37 +0900

    Merge origin/1.8/master into develop
    Conflicts:
    	fuel/app/config/routes.php

--- fuel/app/classes/controller/menu2.php
+++ fuel/app/classes/controller/menu2.php
@@ -7,9 +7,9 @@ class Controller_menu2 extends Controller
 		if(!isset($_SESSION)){
 			session_start();
 		}
-		$_SESSION['id']     = '11';
-		$_SESSION['itemCd'] = 'H11';
-		$_SESSION['area']   = '2号棟　印刷工程';
+		$_SESSION['id']     = '15';
+		$_SESSION['itemCd'] = 'H15';
+		$_SESSION['area']   = '第１工場';
 		
 		$_SESSION['img_top']    = 0; 
 		$_SESSION['img_left']   = 0; 
