7d399dcc51a88a3317608fdb08f11343a7eb97a7
ttkgt86@gmail.com <ttkgt86@gmail.com>
2018-11-08 21:36:09 +0900

    開発環境でプロファイリングを有効化

--- fuel/app/views/html2canvas/index.php
+++ fuel/app/views/html2canvas/index.php
@@ -1,51 +1,34 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<title>html2canvasテスト</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-<script src="html2canvas.min.js"></script>
-<script type="text/javascript">
-	function getDisplayImage(){
-		//html2canvas実行
-		html2canvas(document.getElementById("target")).then(function(canvas) {
-			downloadImage(canvas.toDataURL());
-		});
-	}
-
-	function downloadImage (data) {
-		var fname ="testimage.png";
-		var encdata= atob(data.replace(/^.*,/, ''));
-		var outdata = new Uint8Array(encdata.length);
-		for (var i = 0; i < encdata.length; i++) {
-			outdata[i] = encdata.charCodeAt(i);
-		}
-		var blob = new Blob([outdata], ["image/png"]);
-		
-		if (window.navigator.msSaveBlob) {
-			//IE用
-			window.navigator.msSaveOrOpenBlob(blob, fname);
-		} else {
-			//それ以外？
-			document.getElementById("getImage").href=data;			//base64そのまま設定
-			document.getElementById("getImage").download=fname;		//ダウンロードファイル名設定
-			document.getElementById("getImage").click(); 			//自動クリック
-		}
-	}
-
-	</script>
+  <meta charset="UTF-8">
+  <title>JSで撮るスクリーンショット</title>
 </head>
-	<body>
-		<div>
-			<input id="downloadImageButton" style="width:120px;" type="button" value="画像保存" onclick="getDisplayImage();" >
-			<a id="getImage" href=""  style="display:none;"  download="image.png">画像保存</a>
-		</div>
-		<hr>
-
-		 <div id="target" style="width:500px;height:500px">
-				<img id="work" src="computer_crt_monitor_desktop.png"/>
-			</div>
-			<div style="position:absolute;z-index:10;font-size:28px;top:150px;left:250px;background-color:#FFFFFF;">仕事中</div>
-		</div>
-	</body>
+<body>
+  <h1>JSで撮るスクリーンショット</h1>
+  <h2>導入方法</h2>
+  <ol>
+    <li>html2canvas.jsを読込む</li>
+    <li>html2canvas関数を呼ぶ</li>
+    <li>onrenderedでSSが書き込まれたcanvasを好きなように使う</li>
+  </ol>
+  <h2>注意</h2>
+  <ul>
+    <li>Webサーバーに配置しないとエラーになるかも（firefox30.0でローカルで動作確認）</li>
+    <li>このサンプルではブラウザによっては、ダウンロードできないかも</li>
+    <li>flashやapplet,iframeはうまくキャプチャできない</li>
+  </ul>
+  <a href="" id="ss" download="ss.png">スクリーンショット</a>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+  <script>
+    window.onload = function(){
+      html2canvas(document.body,{
+        onrendered: function(canvas){
+          //aタグのhrefにキャプチャ画像のURLを設定
+          document.getElementById("ss").href = canvas.toDataURL("image/png");
+        }
+      });
+    }
+  </script>
+</body>
 </html>
